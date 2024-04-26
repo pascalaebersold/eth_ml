1c2c3086e9943dca5a01c192443d3b8315946c9c
pascalaebersold <pascal.aebersold@bluewin.ch>
2024-04-26 16:32:19 +0200

    SVR Model

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # utils imports
 from utils import load_config, load_dataset, load_private_test_dataset, print_results, save_results
+from pathlib import Path
 
 # scikit-learn imports
 from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
+from sklearn.metrics import mean_absolute_error
 
 # matplotlib imports
 import matplotlib
@@ -13,9 +18,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # scikit-image import
-from skimage import data, img_as_float
+# from skimage import data, img_as_float
 from skimage import exposure
 
+from PIL import Image
 
 
 if __name__ == "__main__":
@@ -23,21 +29,53 @@ if __name__ == "__main__":
     config = load_config()
 
     # Load dataset: images and corresponding minimum distance values
-    images, distances = load_dataset(config)
-    print(f"[INFO]: Dataset loaded with {len(images)} samples.")
+    images_train, distances_train = load_dataset(config, split="train")
+    print(f"[INFO]: Dataset loaded with {len(images_train)} samples.")
+    images_test, distances_test = load_dataset(config, split="public_test")
+    print(f"[INFO]: Dataset loaded with {len(images_test)} samples.")
 
-    X = np.array(images)
+    X_train, y_train = np.array(images_train), np.array(distances_train)
+    X_test, y_test = np.array(images_test), np.array(distances_test)
 
     scaler = StandardScaler()
+    scaler.fit(X_train)
 
-    X_scaled = scaler.fit_transform(X)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-    # TODO: Your implementation starts here
+    svr_model = SVR(kernel='rbf', C=3, cache_size=8000, shrinking=False, epsilon=0.2)
+    svr_model.fit(X_train_scaled, y_train)
 
-    # possible preprocessing steps ... training the model
+    y_pred_train = svr_model.predict(X_train_scaled)
+    y_pred = svr_model.predict(X_test_scaled)
 
-    # Evaluation
-    # print_results(gt, pred)
+    # show mean squared error (not important)
+    mse_train = mean_squared_error(y_train, y_pred_train)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error (train): {mse_train}")
+    print("Mean Squared Error (test): ", mse)
 
-    # Save the results
-    # save_results(private_test_pred)
+    # show maximum (not important) and mean difference
+    diff_train = abs(y_train - y_pred_train)
+    print(f"maximum difference (train): {diff_train.max()}, mean difference: {diff_train.mean()}")
+    diff = abs(y_test - y_pred)
+    print(f"maximum difference: {diff.max()}, mean difference: {diff.mean()}")
+
+    # compare predicted distance to actual distance
+    """for i in range(len(X_test_scaled)):
+        pred = svr_model.predict(np.array([X_test_scaled[i]]))
+        expected = y_test[i]
+        print(f"actual distance: {expected}, predicted distance: {pred}")
+        input()"""
+
+    # load private test images and guess for yourself
+    """private_test_images = load_private_test_dataset(config)
+    for image in private_test_images:
+        img = scaler.transform(np.array([image]))
+        pred = svr_model.predict(img)
+        print(f"predicted distance: {pred}")
+        print(img.shape)
+        image = image.reshape((30, 30, 3))
+        plt.imshow(image)
+        plt.show()
+        input()"""
--- test_crap.py
+++ test_crap.py
@@ -1,5 +1,74 @@
-import os
+# utils imports
+from utils import load_config, load_dataset, load_private_test_dataset, print_results, save_results
 
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
+# time imports
+from time import time
+
+# import open cv
+import cv2
+
+# scikit-learn imports
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+
+# matplotlib imports
+import matplotlib
+import matplotlib.pyplot as plt
+
+# numpy imports
+import numpy as np
+
+# scikit-image import
+from skimage import data, img_as_float
+from skimage import exposure
+
+def extract_features(image_paths):
+    image = cv2.imread(image_paths)
+    hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    hist = hist.flatten()
+    return hist
+
+
+
+if __name__ == "__main__":
+    # Load configs from "config.yaml"
+    config = load_config()
+
+    # Load dataset: images and corresponding minimum distance values
+    images, distances = load_dataset(config)
+    print(f"[INFO]: Dataset loaded with {len(images)} samples.")
+
+    image_paths = ["/data/train_images/"]
+    #X = np.array(images)
+    y = np.array(distances)
+
+    X = [extract_features(image_path) for image_path in image_paths]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
+
+
+    model = RandomForestClassifier(n_estimators=100, n_jobs=8)
+    start = time()
+    model.fit(X_train, y_train)
+    stop = time()
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+
+    print("[Accuracy]:", acc)
+    print("[Time]:", stop-start)
+
+    # TODO: Your implementation starts here
+
+    # possible preprocessing steps ... training the model
+
+    # Evaluation
+    # print_results(gt, pred)
+
+    # Save the results
+    # save_results(private_test_pred)
