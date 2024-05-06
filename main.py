6cfd93fe0f64644aca122555ee00a5ccb8cd87e4
simonexpf <simonerosapfeiffer@gmail.com>
2024-05-06 12:04:36 +0200

    truncated svd

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import RobustScaler
+from sklearn.model_selection import cross_val_score
+
 
 from sklearn.pipeline import Pipeline
 
+
+from sklearn.svm import NuSVR
+from sklearn.decomposition import TruncatedSVD
 
 # matplotlib imports
 import matplotlib
@@ -42,23 +49,37 @@ if __name__ == "__main__":
     X_test, y_test = np.array(images_test), np.array(distances_test)
 
     
-    scaler = StandardScaler()
-    scaler.fit(X_train)
+    #scaler = StandardScaler()
+    #scaler.fit(X_train)
 
     
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    #X_train_scaled = scaler.transform(X_train)
+    #X_test_scaled = scaler.transform(X_test)
 
-    svr_model = SVR(kernel='rbf', C=3, cache_size=8000, shrinking=False, epsilon=0.05)
-    svr_model.fit(X_train_scaled, y_train)
+    #svr_model = NuSVR(nu = 0.6, C = 3, kernel = 'rbf', degree = 3, gamma = 0.00055, coef0 = 0.0, shrinking = True, cache_size = 2000, tol = 0.001, verbose = False, max_iter = -1)
+    #svr_model.fit(X_train_scaled, y_train)
 
     #pipeline.fit(X_train,y_train)
+    #('pca', PCA(n_components = 100, svd_solver='full')),
+
     
+
+    pipe = Pipeline([
+        ('scaler', RobustScaler(with_centering = True, with_scaling = True, quantile_range = (30.0,70.0), copy = True)), 
+        ('svd', TruncatedSVD(n_components = 90, algorithm = 'randomized', n_oversamples = 12, power_iteration_normalizer = 'none')),
+        ('NuSVR',NuSVR(nu = 0.6, C = 3, kernel = 'rbf', degree = 2, gamma = 0.00055, coef0 = 0.0, shrinking = True, cache_size = 2000, tol = 0.0001, verbose = False, max_iter = -1))
+        ])
     
-    y_pred_train = svr_model.predict(X_train_scaled)
-    y_pred = svr_model.predict(X_test_scaled)
-    #y_pred_train = pipeline.predict(X_train)
-    #y_pred = pipeline.predict(X_test)
+    #perform cross-validation
+    scores = cross_val_score(pipe, X_train, y_train, cv=5, scoring = 'neg_mean_squared_error')
+    print("Cross_validation MSE:", -scores.mean())
+
+    pipe.fit(X_train,y_train)
+
+    #y_pred_train = svr_model.predict(X_train_scaled)
+    #y_pred = svr_model.predict(X_test_scaled)
+    y_pred_train = pipe.predict(X_train)
+    y_pred = pipe.predict(X_test)
     
     
 
@@ -73,6 +94,7 @@ if __name__ == "__main__":
     print(f"maximum difference (train): {diff_train.max()}, mean difference: {diff_train.mean()}")
     diff = abs(y_test - y_pred)
     print(f"maximum difference: {diff.max()}, mean difference: {diff.mean()}")
+    print_results(y_test,y_pred)
 
     # compare predicted distance to actual distance
     """for i in range(len(X_test_scaled)):
@@ -82,7 +104,7 @@ if __name__ == "__main__":
         input()"""
 
     # load private test images and guess for yourself
-    private_test_images = load_private_test_dataset(config)
+    '''private_test_images = load_private_test_dataset(config)
     for image in private_test_images:
         img = scaler.transform(np.array([image]))
         pred = svr_model.predict(img)
@@ -91,4 +113,4 @@ if __name__ == "__main__":
         image = image.reshape((300//config["downsample_factor"], 300//config["downsample_factor"], 3))
         plt.imshow(image)
         plt.show()
-        input()
+        input()'''
